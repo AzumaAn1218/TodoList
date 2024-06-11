8f699e994d4c01abace7a5a70dc236481ea0f31a
AzumaAn1218 <nyotarou1218@gmail.com>
2024-06-11 21:38:26 +0900

    TodoListの原形まで

--- TodoList/ContentView.swift
+++ TodoList/ContentView.swift
@@ -9,77 +9,32 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
+    @State var selection = 1
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
-                    }
+        TabView(selection: $selection) {
+
+            CalenderPage()
+                .tabItem {
+                    Label("カレンダー", systemImage: "1.circle")
                 }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
+                .tag(1)
+
+            TodoListPage()
+                .tabItem {
+                    Label("タスク", systemImage: "2.circle")
                 }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
+                .tag(2)
+
+            AnalyticsPage()
+                .tabItem {
+                    Label("アナリティクス", systemImage: "3.circle")
                 }
-            }
-            Text("Select an item")
-        }
-    }
+                .tag(3)
 
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
+        } // TabView ここまで
+    } // bodyここまで
+} // ContentViewここまで
 
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-}
-
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
 
 #Preview {
     ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
--- TodoList/TodoListPage.swift
+++ TodoList/TodoListPage.swift
@@ -7,9 +7,74 @@
 
 import SwiftUI
 
+struct ToDoItem {
+    var isChecked: Bool
+    var task: String
+}
+
 struct TodoListPage: View {
+    @State var newTask: String = ""
+    @State var todoLists: [ToDoItem] = []
+    @State var showAlert = false
+    @State var text = ""
+    
+    // 現在の日付を取得する関数
+    func getCurrentDate() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy/MM/dd"
+        return dateFormatter.string(from: Date())
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text(getCurrentDate())
+            ForEach(todoLists.indices, id: \.self) { index in
+                HStack {
+                    Button(action: {
+                        todoLists[index].isChecked.toggle()
+                    }, label: {
+                        Image(systemName:
+                                todoLists[index].isChecked ? "checkmark.square" : "square"
+                        )
+                        .imageScale(.large)
+                        .foregroundStyle(.pink)
+                    })
+                    if todoLists[index].isChecked {
+                        Text(todoLists[index].task)
+                            .strikethrough()
+                    } else {
+                        Text(todoLists[index].task)
+                    }
+                }
+                .padding(.top, 1)
+                .padding(.leading, 20)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            }
+            
+            Spacer()
+            Button {
+                showAlert = true
+            } label: {
+                Text("＋")
+                    .font(.title)
+                    .foregroundColor(Color.white)
+                    .frame(width:80, height:80)
+                    .background(Color("bottonColor"))
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            }
+        }
+        .alert("タスクの追加", isPresented: $showAlert, actions: {
+            TextField("新規タスク", text: $newTask)
+            Button("キャンセル", action: {})
+            Button("追加する", action: {
+                if !newTask.isEmpty {
+                    todoLists.append(
+                        ToDoItem(isChecked: false, task: newTask)
+                    )
+                    newTask = ""
+                }
+            })
+        })
     }
 }
 
